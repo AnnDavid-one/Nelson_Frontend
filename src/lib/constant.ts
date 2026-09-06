c8982386c4832f6cdafc2c7ecd890b7773dfaff0
DavidHartUlea <duchenna59@gmail.com>
2026-09-06 19:44:30 +0100

    changed the email

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,4 @@
 //src/lib/constant.ts
 
 export const WHATSAPP_NUMBER = "2347063613995";
-export const CONTACT_EMAIL = "duchenna59@gmail.com";
+export const CONTACT_EMAIL = "nelsonbello46@gmail.com";
